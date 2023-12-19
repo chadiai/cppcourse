372a56054db3802518b21d47a23ab7fe2e1923f0
chadiai <r0786882@student.thomasmore.be>
2023-12-19 17:52:14 +0100

    solution first challenge

---
+++ Challenge1/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int fav;
+    cout << "nter your favorite number between 1 and 100: ";
+    cin >> fav;
+    cout << "Amazing!! That's my favorite number too!" << endl;
+    cout << "No really!!, " << fav << " is my favorite number!" << endl;
+    return 0;
+}
